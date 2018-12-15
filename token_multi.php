8bbb79aad3fc5d1e8b0ddc90a4977863b2224807
dbgid <45901336+dbgid@users.noreply.github.com>
2018-12-15 17:33:08 +0000

    Add files via upload

---
+++ token_multi.php
@@ -0,0 +1,11 @@
+<?php
+/**This is Config File
+* this config for multiple account
+* this config only work for watching videos
+*/
+$token1='xxxxxxxxxxxxx';
+$token2='xxxxxxxxxxxxx';
+$token3='xxxxxxxxxxxxx';
+$token4='xxxxxxxxxxxxx';
+$token5='xxxxxxxxxxxxx';
+?>
